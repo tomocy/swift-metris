e54c7484292243cf9819aad32a8e4d9a8507322e
tomocy <tomocy.dev@gmail.com>
2023-08-24 02:34:38 +0900

    Add AppDelegate

--- swift-metris/main.swift
+++ swift-metris/main.swift
@@ -1,11 +1,27 @@
-//
-//  main.swift
-//  swift-metris
-//
-//  Created by tomocy on 2023/08/24.
-//
-
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+let app = NSApplication.shared;
+let delegate = AppDelegate();
+app.delegate = delegate;
 
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv);
+
+class AppDelegate : NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSLog("AppDelegate: Finished launching");
+        
+        window = NSWindow(
+                   contentRect: NSRect(x: 0, y: 0, width: 480, height: 270),
+                   styleMask: [.titled, .miniaturizable, .closable],
+                   backing: .buffered,
+                   defer: false
+        )
+        window!.title = "swift metris"
+        
+        window!.orderFrontRegardless();
+        window!.center();
+    }
+    
+    private var window: NSWindow?;
+}
