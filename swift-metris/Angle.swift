af4d82b19288f6f50433adf06015f72a6b27d76c
tomocy <tomocy.dev@gmail.com>
2023-10-11 15:34:14 +0900

    Implement Angle@SignedNumeric

--- swift-metris/Angle.swift
+++ swift-metris/Angle.swift
@@ -1,46 +1,56 @@
 // tomocy
 
 struct Angle {
+    typealias Raw = Float
+
+    init(degree: Raw) {
+        raw = degree * .pi / 180
+    }
+
+    init(radian: Raw) {
+        raw = radian
+    }
+
+    func inDegree() -> Raw {
+        return raw * 180 / .pi
+    }
+
+    func inRadian() -> Raw {
+        return raw
+    }
+
+    // The raw value is stored in radian.
+    private var raw: Raw = 0
+}
+
+extension Angle : SignedNumeric {
+    typealias Magnitude = Raw.Magnitude
+    typealias IntegerLiteralType = Raw.IntegerLiteralType
+
     static func +(left: Self, right: Self) -> Self {
-        return Self(radian: left.raw + right.raw)
+        Self(radian: left.raw + right.raw)
+    }
+
+    static func -(left: Self, right: Self) -> Self {
+        Self(radian: left.raw - right.raw)
     }
 
     static func *(left: Self, right: Self) -> Self {
-        return Self(radian: left.raw * right.raw)
-    }
-
-    static func *(left: Self, right: Float) -> Self {
-        return Self(radian: left.raw * right)
-    }
-
-    static func +=(left: inout Self, right: Self) {
-        left = left + right
+        Self(radian: left.raw * right.raw)
     }
 
     static func *=(left: inout Self, right: Self) {
         left = left * right
     }
 
-    static func *=(left: inout Self, right: Float) {
-        left = left * right
+    init(integerLiteral value: Raw.IntegerLiteralType) {
+        self.init(degree: Raw.init(integerLiteral: value))
     }
 
-    init(degree: Float) {
-        raw = degree * .pi / 180
+    init?<T>(exactly source: T) where T : BinaryInteger {
+        guard let radian = Raw.init(exactly: source) else { return nil }
+        self.init(radian: radian)
     }
 
-    init(radian: Float) {
-        raw = radian
-    }
-
-    func inDegree() -> Float {
-        return raw * 180 / .pi
-    }
-
-    func inRadian() -> Float {
-        return raw
-    }
-
-    // The raw value is stored in radian.
-    private var raw: Float = 0
+    var magnitude: Raw.Magnitude { raw.magnitude }
 }
