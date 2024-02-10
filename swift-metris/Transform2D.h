2d24f63990bca3e206c73d492161bc72db5bff18
tomocy <tomocy.dev@gmail.com>
2024-02-10 22:38:33 +0900

    Fix for Transform2D to make rotate transformation in column-major order

--- swift-metris/Transform2D.h
+++ swift-metris/Transform2D.h
@@ -14,36 +14,47 @@ public:
 
     float3 apply(float3 position) const
     {
-        const auto t = metal::float3x3(
-            float3(1, 0, 0),
-            float3(0, 1, 0),
-            float3(translate.x, translate.y, 1)
-        );
-
-        auto r = metal::float3x3(1);
-        {
-            const float s = metal::sin(rotate);
-            const float c = metal::cos(rotate);
-            r = metal::float3x3(
-                float3(c, s, 0),
-                float3(-s, c, 0),
-                float3(0, 0, 1)
-            );
-        }
-
-        const auto s = metal::float3x3(
-            float3(scale.x, 0, 0),
-            float3(0, scale.y, 0),
-            float3(0, 0, 1)
-        );
-
-        position = s * position;
-        position = r * position;
-        position = t * position;
+        // In MSL, matrix are constructed in column-major order.
+        // Therefore in applying transformations,
+        // you should proceed from right to left: scale, rotate and then translate.
+        position = toScale() * position;
+        position = toRotate() * position;
+        position = toTranslate() * position;
 
         return position;
     }
 
+protected:
+    metal::float3x3 toTranslate() const
+    {
+        return metal::float3x3(
+            float3(1, 0, 0),
+            float3(0, 1, 0),
+            float3(translate.x, translate.y, 1)
+        );
+    }
+
+    metal::float3x3 toRotate() const
+    {
+        const float s = metal::sin(rotate);
+        const float c = metal::cos(rotate);
+        return metal::float3x3(
+            float3(c, -s, 0),
+            float3(s, c, 0),
+            float3(0, 0, 1)
+        );
+    }
+
+    metal::float3x3 toScale() const
+    {
+        return metal::float3x3(
+            float3(scale.x, 0, 0),
+            float3(0, scale.y, 0),
+            float3(0, 0, 1)
+        );
+    }
+
+public:
     float2 translate = float2(0, 0);
     float rotate = 0;
     float2 scale = float2(0, 0);
