3197a156dda87cc457108359560b14ca5a8fa6cd
tomocy <tomocy.dev@gmail.com>
2024-02-11 14:23:08 +0900

    Let IndexedPrimitive implement MTLRenderCommandEncodableWithIndexedBufferAt

--- swift-metris/IndexedPrimitive.swift
+++ swift-metris/IndexedPrimitive.swift
@@ -3,11 +3,11 @@
 import Metal
 
 struct IndexedPrimitive {
-    var lastIndex: Int {
-        vertices.count - 1
-    }
-
+    var verticesSize: Int { MemoryLayout<Vertex>.stride * vertices.count }
     private var vertices: [Vertex] = []
+
+    var indicesSize: Int { MemoryLayout<UInt16>.stride * indices.count }
+    var lastIndex: Int { vertices.count - 1 }
     private var indices: [UInt16] = []
 }
 
@@ -22,30 +22,47 @@ protocol IndexedPrimitiveAppendable {
     func append(to primitive: inout IndexedPrimitive)
 }
 
-extension IndexedPrimitive: MTLFrameRenderCommandEncodableAt {
-    func encode(to encoder: MTLRenderCommandEncoder, at index: Int, in frame: MTLRenderFrame) {
+extension IndexedPrimitive: MTLRenderCommandEncodableWithIndexedBufferAt {
+    func encode(to encoder: MTLRenderCommandEncoder, with buffer: MTLIndexedBuffer, at index: Int) {
         if (vertices.isEmpty) {
             return
         }
 
-        let vertexBuffer = encoder.device.makeBuffer(
-            bytes: vertices,
-            length: MemoryLayout<Vertex>.stride * vertices.count,
-            options: .storageModeShared
-        )!
-        encoder.setVertexBuffer(vertexBuffer, offset: 0, index: index)
+        buffer.data.contents().copyMemory(
+            from: vertices,
+            byteCount: buffer.data.length
+        )
+        encoder.setVertexBuffer(buffer.data, offset: 0, index: index)
 
-        let indexBuffer = encoder.device.makeBuffer(
-            bytes: indices,
-            length: MemoryLayout<UInt16>.stride * indices.count,
-            options: .storageModeShared
-        )!
+        buffer.index.contents().copyMemory(
+            from: indices,
+            byteCount: buffer.index.length
+        )
         encoder.drawIndexedPrimitives(
             type: .triangle,
             indexCount: indices.count,
             indexType: .uint16,
-            indexBuffer: indexBuffer,
+            indexBuffer: buffer.index,
             indexBufferOffset: 0
+        )
+    }
+}
+
+extension IndexedPrimitive: MTLFrameRenderCommandEncodableAt {
+    func encode(to encoder: MTLRenderCommandEncoder, at index: Int, in frame: MTLRenderFrame) {
+        encode(
+            to: encoder,
+            with: .init(
+                data: encoder.device.makeBuffer(
+                    length: verticesSize,
+                    options: .storageModeShared
+                )!,
+                index: encoder.device.makeBuffer(
+                    length: indicesSize,
+                    options: .storageModeShared
+                )!
+            ),
+            at: index
         )
     }
 }
