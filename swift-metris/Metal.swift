cb88c34d5eb23ce4590aa7830ad3dad313bc81e2
tomocy <tomocy.dev@gmail.com>
2024-02-11 17:06:42 +0900

    Add MTLSizedBuffers

--- swift-metris/Metal.swift
+++ swift-metris/Metal.swift
@@ -11,6 +11,36 @@ struct MTLIndexedBuffer {
     var index: MTLBuffer
 }
 
+struct MTLSizedBuffers {
+    init(options: MTLResourceOptions = []) {
+        self.options = options
+    }
+
+    mutating func take(at index: Int, of size: Int, with device: MTLDevice) -> MTLBuffer {
+        if !has(at: index, of: size) {
+            buffers[index] = device.makeBuffer(
+                length: size,
+                options: options
+            )
+            NSLog("buffer: generated: size: \(size)")
+        }
+
+        return buffers[index]!
+    }
+
+    func has(at index: Int, of size: Int) -> Bool {
+        return buffers.contains(
+            where: { (i, buffer) in
+                return i == index
+                    && buffer.length == size
+            }
+        )
+    }
+
+    private var options: MTLResourceOptions = []
+    private var buffers: [Int: MTLBuffer] = [:]
+}
+
 protocol MTLRenderCommandEncodableWithAt {
     func encode(to encoder: MTLRenderCommandEncoder, with buffer: MTLBuffer, at index: Int)
 }
