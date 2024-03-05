5d8c8bf86bc24330ee04fb7a0535c4af52c71a9e
tomocy <tomocy.dev@gmail.com>
2024-03-05 23:08:36 +0900

    Add IndexedPrimitive3D

--- swift-metris/Primitive.swift
+++ swift-metris/Primitive.swift
@@ -50,6 +50,50 @@ extension IndexedPrimitive2D: MTLRenderCommandEncodableToIndexedAt {
     }
 }
 
+struct IndexedPrimitive3D {
+    var vertices: [Vertex3D] = []
+    var indices: [UInt16] = []
+
+    var lastIndex: Int { vertices.count - 1 }
+}
+
+extension IndexedPrimitive3D {
+    mutating func append(vertices: [Vertex3D], indices: [UInt16]) {
+        self.vertices += vertices
+        self.indices += indices
+    }
+}
+
+extension IndexedPrimitive3D: MTLRenderCommandEncodableToIndexedAt {
+    func encode(with encoder: MTLRenderCommandEncoder, to buffer: MTLIndexedBuffer, at index: Int) {
+        if (vertices.isEmpty) {
+            return
+        }
+
+        do {
+            buffer.data.contents().copyMemory(
+                from: vertices,
+                byteCount: buffer.data.length
+            )
+            encoder.setVertexBuffer(buffer.data, offset: 0, index: index)
+        }
+
+        do {
+            buffer.index.contents().copyMemory(
+                from: indices,
+                byteCount: buffer.index.length
+            )
+            encoder.drawIndexedPrimitives(
+                type: .triangle,
+                indexCount: indices.count,
+                indexType: .uint16,
+                indexBuffer: buffer.index,
+                indexBufferOffset: 0
+            )
+        }
+    }
+}
+
 extension Array {
     var size: Int { MemoryLayout<Element>.stride * count }
 }
