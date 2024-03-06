795ad521cf32dc3983621ec498cee6c2c112d3ad
tomocy <tomocy.dev@gmail.com>
2024-03-07 03:04:09 +0900

    Replace World2D with World3D

--- swift-metris/World.metal
+++ swift-metris/World.metal
@@ -4,25 +4,6 @@
 #include "Fragment.h"
 #include "Vertex.h"
 
-namespace World2D {
-    vertex Fragment shadeVertex(
-        constant Camera2D* const camera [[buffer(0)]],
-        constant Vertex2D* const vs [[buffer(1)]],
-        const uint id [[vertex_id]]
-    )
-    {
-        const constant auto* v = &vs[id];
-
-        auto position = v->resolvePosition();
-        position = camera->applyTransformTo(position);
-
-        return {
-            .position = position,
-            .color = v->color,
-        };
-    }
-}
-
 namespace World3D {
     vertex Fragment shadeVertex(
         constant Camera3D* const camera [[buffer(0)]],
--- swift-metris/World.swift
+++ swift-metris/World.swift
@@ -7,46 +7,6 @@ protocol World: MTLRenderPipelineDescriable, MTLFrameRenderCommandEncodable {
     var metris: Metris { get }
 }
 
-class World2D: World {
-    init(size: CGSize) {
-        do {
-            let halfSize = SIMD2<Float>.init(size) / 2
-            camera = .init(
-                projection: .orthogonal(
-                    top: halfSize.y, bottom: -halfSize.y,
-                    left: -halfSize.x, right: halfSize.x
-                ),
-                transform: .init(
-                    translate: halfSize
-                )
-            )
-        }
-
-        metris = .init(size: size)
-        metris.start()
-    }
-
-    var camera: Camera2D
-    var metris: Metris
-}
-
-extension World2D: MTLRenderPipelineDescriable {
-    func describe(with device: MTLDevice, to descriptor: MTLRenderPipelineDescriptor) {
-        let lib = device.makeDefaultLibrary()!
-
-        descriptor.vertexFunction = lib.makeFunction(name: "World2D::shadeVertex")!
-        descriptor.fragmentFunction = lib.makeFunction(name: "shadeFragment")!
-    }
-}
-
-
-extension World2D: MTLFrameRenderCommandEncodable {
-    func encode(with encoder: MTLRenderCommandEncoder, in frame: MTLRenderFrame) {
-        camera.encode(with: encoder, at: 0, in: frame)
-        metris.encode(with: encoder, at: 1, in: frame)
-    }
-}
-
 class World3D: World {
     init(size: CGSize) {
         do {
