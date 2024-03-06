393b0be93d88eb468cd4538034ea7d34c2a9ed7e
tomocy <tomocy.dev@gmail.com>
2024-03-06 16:14:40 +0900

    Enable Vertex to transform itself {with, by} the given one

--- swift-metris/Vertex.swift
+++ swift-metris/Vertex.swift
@@ -69,6 +69,16 @@ extension Vertex3D {
 }
 
 extension Vertex3D {
+    mutating func place(at position: SIMD3<Float>) {
+        self.position = position
+    }
+
+    func placed(at position: SIMD3<Float>) -> Self {
+        var next = self
+        next.place(at: position)
+        return next
+    }
+
     mutating func colorize(with color: SIMD4<Float>) {
         self.color = color
     }
@@ -76,6 +86,26 @@ extension Vertex3D {
     func colorized(with color: SIMD4<Float>) -> Self {
         var next = self
         next.colorize(with: color)
+        return next
+    }
+
+    mutating func transform(with transform: Transform3D) {
+        self.transform = transform
+    }
+
+    mutating func transform(by delta: Transform3D) {
+        transform.transform(by: delta)
+    }
+
+    func transformed(with transform: Transform3D) -> Self {
+        var next = self
+        next.transform(with: transform)
+        return next
+    }
+
+    func transformed(by delta: Transform3D) -> Self {
+        var next = self
+        next.transform(by: delta)
         return next
     }
 }
