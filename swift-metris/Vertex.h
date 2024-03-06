2c958e1da4a60c2930e45b0abb76d3287c2865f1
tomocy <tomocy.dev@gmail.com>
2024-03-07 03:11:18 +0900

    Replace Vertex2D with Vertex3D

--- swift-metris/Vertex.h
+++ swift-metris/Vertex.h
@@ -4,23 +4,6 @@
 
 #include "Transform.h"
 
-struct Vertex2D {
-public:
-    float4 resolvePosition(const float z = 1, const float w = 1) const constant {
-        return applyTransformTo(float4(position, z, w));
-    }
-
-    float4 applyTransformTo(const float4 position) const constant {
-        return transform.apply(position);
-    }
-
-
-public:
-    float2 position = float2(0, 0);
-    float4 color = float4(0, 0, 0, 1);
-    Transform2D transform = {};
-};
-
 struct Vertex3D {
 public:
     float4 resolvePosition(const float w = 1) const constant {
--- swift-metris/Vertex.swift
+++ swift-metris/Vertex.swift
@@ -2,60 +2,6 @@
 
 import CoreGraphics
 
-struct Vertex2D {
-    var position: SIMD2<Float> = .init(0, 0)
-    var color: SIMD4<Float> = .init(0, 0, 0, 1)
-    var transform: Transform2D = .init()
-}
-
-extension Vertex2D {
-    init(at position: SIMD2<Float>) {
-        self.position = position
-    }
-}
-
-extension Vertex2D {
-    mutating func place(at position: SIMD2<Float>) {
-        self.position = position
-    }
-
-    func placed(at position: SIMD2<Float>) -> Self {
-        var next = self
-        next.place(at: position)
-        return next
-    }
-
-    mutating func colorize(with color: SIMD4<Float>) {
-        self.color = color
-    }
-
-    func colorized(with color: SIMD4<Float>) -> Self {
-        var next = self
-        next.colorize(with: color)
-        return next
-    }
-
-    mutating func transform(with transform: Transform2D) {
-        self.transform = transform
-    }
-
-    mutating func transform(by delta: Transform2D) {
-        transform.transform(by: delta)
-    }
-
-    func transformed(with transform: Transform2D) -> Self {
-        var next = self
-        next.transform(with: transform)
-        return next
-    }
-
-    func transformed(by delta: Transform2D) -> Self {
-        var next = self
-        next.transform(by: delta)
-        return next
-    }
-}
-
 struct Vertex3D {
     var position: SIMD3<Float> = .init(0, 0, 0)
     var color: SIMD4<Float> = .init(0, 0, 0, 1)
