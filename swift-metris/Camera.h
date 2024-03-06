8b9e9259452c4da17d6e06b132b51743f598a02c
tomocy <tomocy.dev@gmail.com>
2024-03-06 17:12:17 +0900

    Enable metal/Camera3D to resolve its position

--- swift-metris/Camera.h
+++ swift-metris/Camera.h
@@ -20,6 +20,14 @@ public:
 
 struct Camera3D {
 public:
+    float3 applyTransformTo(const float3 position) const constant {
+        auto result = position;
+        result = transform.apply(result);
+        result = projection.apply(result);
+        return result;
+    }
+
+public:
     Transform3D projection = {};
     Transform3D transform = {};
 };
