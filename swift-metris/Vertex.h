d12fc0055cd61ace4ac45c63101baa56042abad2
tomocy <tomocy.dev@gmail.com>
2024-03-06 17:11:10 +0900

    Enable metal/Vertex3D to resolve its position

--- swift-metris/Vertex.h
+++ swift-metris/Vertex.h
@@ -23,6 +23,15 @@ public:
 
 struct Vertex3D {
 public:
+    float3 resolvePosition() const constant {
+        return applyTransformTo(position);
+    }
+
+    float3 applyTransformTo(const float3 position) const constant {
+        return transform.apply(position);
+    }
+
+public:
     float3 position = float3(0, 0, 0);
     float4 color = float4(0, 0, 0, 1);
     Transform3D transform = {};
