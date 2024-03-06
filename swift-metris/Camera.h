e7dd3f7be4fc4140bdc5ed67e19d1f326949e091
tomocy <tomocy.dev@gmail.com>
2024-03-07 03:09:09 +0900

    Replace Camera2D with Camera3D

--- swift-metris/Camera.h
+++ swift-metris/Camera.h
@@ -4,20 +4,6 @@
 
 #include "Transform.h"
 
-struct Camera2D {
-public:
-    float4 applyTransformTo(const float4 position) const constant {
-        auto result = position;
-        result = transform.apply(result);
-        result = projection.apply(result);
-        return result;
-    }
-
-public:
-    Transform2D projection = {};
-    Transform2D transform = {};
-};
-
 struct Camera3D {
 public:
     float4 applyTransformTo(const float4 position) const constant {
--- swift-metris/Camera.swift
+++ swift-metris/Camera.swift
@@ -2,68 +2,6 @@
 
 import Metal
 
-struct Camera2D {
-    init(
-        projection: Transform2D = .init(),
-        transform: Transform2D = .init()
-    ) {
-        self.projection = projection
-        self.transform = transform
-    }
-
-    var projection: Transform2D {
-        get { state.projection }
-        set { state.projection = newValue }
-    }
-    var transform: Transform2D {
-        get { state.transform }
-        set { state.transform = newValue }
-    }
-
-    private var state: MTLRenderState = .init()
-
-    private var frameBuffers: MTLSizedBuffers = .init(options: .storageModeShared)
-}
-
-extension Camera2D {
-    private struct MTLRenderState {
-        static var stride: Int { MemoryLayout<Self>.stride }
-
-        var projection: Transform2D = .init()
-        var transform: Transform2D = .init()
-    }
-}
-
-extension Camera2D: MTLFrameRenderCommandEncodableAt {
-    mutating func encode(with encoder: MTLRenderCommandEncoder, at index: Int, in frame: MTLRenderFrame) {
-        encode(
-            with: encoder,
-            to: frameBuffers.take(
-                at: frame.id,
-                of: type(of: state).stride,
-                with: encoder.device
-            ),
-            at: index
-        )
-    }
-}
-
-
-extension Camera2D: MTLRenderCommandEncodableToAt {
-    func encode(with encoder: MTLRenderCommandEncoder, to buffer: MTLBuffer, at index: Int) {
-        var state = state
-        state.transform = state.transform.inversed(rotate: false, scale: false)
-
-        withUnsafeBytes(of: state, { body in
-            buffer.contents().copyMemory(
-                from: body.baseAddress!,
-                byteCount: body.count
-            )
-            encoder.setVertexBuffer(buffer, offset: 0, index: index)
-        })
-    }
-}
-
 struct Camera3D {
     init(
         projection: Transform3D = .init(),
@@ -113,6 +51,8 @@ extension Camera3D: MTLFrameRenderCommandEncodableAt {
 extension Camera3D: MTLRenderCommandEncodableToAt {
     func encode(with encoder: MTLRenderCommandEncoder, to buffer: MTLBuffer, at index: Int) {
         var state = state
+
+        // Camera should move invertedly to translate itself in the same direction as others.
         state.transform = state.transform.inversed(rotate: false, scale: false)
 
         withUnsafeBytes(of: state, { body in
