e2d7f4e86b9a80c08da388f576c764664291af6e
tomocy <tomocy.dev@gmail.com>
2024-03-07 14:31:28 +0900

    Replace Transform2D with Transform3D

--- swift-metris/Transform.h
+++ swift-metris/Transform.h
@@ -4,64 +4,6 @@
 
 #include <metal_stdlib>
 
-struct Transform2D {
-public:
-    float4 apply(const float4 position) const constant
-    {
-        const auto transform = *this;
-        return transform.apply(position);
-    }
-
-    float4 apply(const float4 position) const
-    {
-        auto result = position.xyz;
-
-        // In MSL, matrix are constructed in column-major order.
-        // Therefore in applying transformations,
-        // you should proceed from right to left: scale, rotate and then translate.
-        result = toScale() * result.xyz;
-        result = toRotate() * result.xyz;
-        result = toTranslate() * result.xyz;
-
-        return float4(result, 1);
-    }
-
-protected:
-    metal::float3x3 toTranslate() const
-    {
-        return metal::float3x3(
-            float3(1, 0, 0),
-            float3(0, 1, 0),
-            float3(translate.x, translate.y, 1)
-        );
-    }
-
-    metal::float3x3 toRotate() const
-    {
-        const float s = metal::sin(rotate);
-        const float c = metal::cos(rotate);
-        return metal::float3x3(
-            float3(c, -s, 0),
-            float3(s, c, 0),
-            float3(0, 0, 1)
-        );
-    }
-
-    metal::float3x3 toScale() const
-    {
-        return metal::float3x3(
-            float3(scale.x, 0, 0),
-            float3(0, scale.y, 0),
-            float3(0, 0, 1)
-        );
-    }
-
-public:
-    float2 translate = float2(0, 0);
-    float rotate = 0;
-    float2 scale = float2(0, 0);
-};
-
 struct Transform3D {
 public:
     enum class Axis {
@@ -75,6 +17,10 @@ public:
     {
         auto result = position;
 
+        // In MSL, matrix are constructed in column-major order.
+        // Therefore in applying transformations,
+        // you should proceed from right to left: scale, rotate and then translate.
+        //
         result = toScale() * result;
         result = toRotateAround(Axis::Z) * result;
         result = toTranslate() * result;
