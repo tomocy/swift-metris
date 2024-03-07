75c51e2d314f4d06302c7134a7a4b86ace698898
tomocy <tomocy.dev@gmail.com>
2024-03-07 17:17:26 +0900

    Add D3.World

--- swift-metris/World.metal
+++ swift-metris/World.metal
@@ -4,6 +4,25 @@
 #include "Fragment.h"
 #include "Vertex.h"
 
+namespace D3 {
+    vertex Fragment shadeVertex(
+        constant Camera* const camera [[buffer(0)]],
+        constant Vertex* const vs [[buffer(1)]],
+        const uint id [[vertex_id]]
+    )
+    {
+        const constant auto* v = &vs[id];
+
+        auto position = v->resolvePosition();
+        position = camera->applyTransformTo(position);
+
+        return {
+            .position = position,
+            .color = v->color,
+        };
+    }
+}
+
 namespace World3D {
     vertex Fragment shadeVertex(
         constant D3::Camera* const camera [[buffer(0)]],
--- swift-metris/World.swift
+++ swift-metris/World.swift
@@ -7,6 +7,51 @@ protocol World: MTLRenderPipelineDescriable, MTLFrameRenderCommandEncodable {
     var metris: Metris { get }
 }
 
+extension D3 {
+    class World {
+        init(size: CGSize) {
+            do {
+                let halfSize = SIMD2<Float>.init(size) / 2
+                let halfDepth = halfSize.max()
+                camera = .init(
+                    projection: .orthogonal(
+                        top: halfSize.y, bottom: -halfSize.y,
+                        left: -halfSize.x, right: halfSize.x,
+                        near: -halfDepth, far: halfDepth
+                    ),
+                    transform: .init(
+                        translate: .init(halfSize, 0)
+                    )
+                )
+            }
+
+            metris = .init(size: size)
+            metris.start()
+        }
+
+        var camera: Camera
+        var metris: Metris
+    }
+}
+
+extension D3.World: World {}
+
+extension D3.World: MTLRenderPipelineDescriable {
+    func describe(with device: MTLDevice, to descriptor: MTLRenderPipelineDescriptor) {
+        let lib = device.makeDefaultLibrary()!
+
+        descriptor.vertexFunction = lib.makeFunction(name: "D3::shadeVertex")!
+        descriptor.fragmentFunction = lib.makeFunction(name: "shadeFragment")!
+    }
+}
+
+extension D3.World: MTLFrameRenderCommandEncodable {
+    func encode(with encoder: MTLRenderCommandEncoder, in frame: MTLRenderFrame) {
+        camera.encode(with: encoder, at: 0, in: frame)
+        metris.encode(with: encoder, at: 1, in: frame)
+    }
+}
+
 class World3D: World {
     init(size: CGSize) {
         do {
