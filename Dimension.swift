f6145011fb3aec8e9540de2899614cccbbbe8119
tomocy <tomocy.dev@gmail.com>
2024-03-07 15:54:10 +0900

    Add D3.Transform

--- Dimension.swift
+++ Dimension.swift
@@ -1,3 +1,13 @@
 // tomocy
 
+import simd
+
+protocol DimensionalPrecision: Encodable, Decodable, FloatingPoint, SIMDScalar {}
+
+extension Float: DimensionalPrecision {}
+
 enum D3 {}
+
+extension D3 {
+    typealias Storage = SIMD3
+}
--- swift-metris/Transform.swift
+++ swift-metris/Transform.swift
@@ -2,6 +2,115 @@
 
 import simd
 
+extension D3 {
+    struct Transform<Precision: DimensionalPrecision> {
+        var translate: Measure = .init(0, 0, 0)
+        var rotate: Measure = .init(0, 0, 0)
+        var scale: Measure = .init(0, 0, 0)
+    }
+}
+
+extension D3.Transform {
+    typealias Measure = D3.Storage<Precision>
+}
+
+extension D3.Transform {
+    static func orthogonal(
+        top: Precision, bottom: Precision,
+        left: Precision, right: Precision,
+        near: Precision, far: Precision
+    ) -> Self {
+        return .init(
+            translate: Measure.init(
+                (left + right) / -2,
+                (bottom + top) / -2,
+                (near + far) / -2
+            ),
+            scale: Measure.init(
+                2 / (right - left),
+                2 / (top - bottom),
+                2 / (near - far)
+            )
+        )
+    }
+}
+
+extension D3.Transform {
+    mutating func translate(with translate: Measure) {
+        self.translate = translate
+    }
+
+    mutating func translate(by delta: Measure) {
+        self.translate += delta
+    }
+
+    func translated(with translate: Measure) -> Self {
+        return mapState(self) { $0.translate(with: translate) }
+    }
+
+    func translated(by delta: Measure) -> Self {
+        return mapState(self) { $0.translate(by: delta) }
+    }
+}
+
+extension D3.Transform {
+    mutating func rotate(with rotate: Measure) {
+        self.rotate = rotate
+    }
+
+    mutating func rotate(by delta: Measure) {
+        self.rotate += delta
+    }
+
+    func rotated(with rotate: Measure) -> Self {
+        return mapState(self) { $0.rotate(with: rotate) }
+    }
+
+    func rotated(by delta: Measure) -> Self {
+        return mapState(self) { $0.rotate(by: delta) }
+    }
+}
+
+extension D3.Transform {
+    mutating func scale(with scale: Measure) {
+        self.scale = scale
+    }
+
+    mutating func scale(by delta: Measure) {
+        self.scale *= delta
+    }
+
+    func scaled(with scale: Measure) -> Self {
+        return mapState(self) { $0.scale(with: scale) }
+    }
+
+    func scaled(by delta: Measure) -> Self {
+        return mapState(self) { $0.scale(by: delta) }
+    }
+}
+
+extension D3.Transform {
+    mutating func inverse(translate: Bool = true, rotate: Bool = true, scale: Bool = true) {
+        if (translate) {
+            self.translate *= -1
+        }
+
+        if (rotate) {
+            self.rotate *= -1
+        }
+
+        if (scale) {
+            self.scale *= -1
+        }
+    }
+
+    func inversed(translate: Bool = true, rotate: Bool = true, scale: Bool = true) -> Self {
+        return mapState(self) {
+            $0.inverse(translate: translate, rotate: rotate, scale: scale)
+        }
+    }
+}
+
 struct Transform3D {
     var translate: Translate = .init(0, 0, 0)
     var rotate: Rotate = .init(0, 0, 0)
