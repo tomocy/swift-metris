ceff8153505c9a017b250f623bcfb284d7a93afb
tomocy <tomocy.dev@gmail.com>
2024-03-07 17:13:41 +0900

    Replace Camera3D with D3.Camera

--- swift-metris/Camera.h
+++ swift-metris/Camera.h
@@ -19,17 +19,3 @@ public:
     Transform transform = {};
 };
 };
-
-struct Camera3D {
-public:
-    float4 applyTransformTo(const float4 position) const constant {
-        auto result = position;
-        result = transform.apply(result);
-        result = projection.apply(result);
-        return result;
-    }
-
-public:
-    D3::Transform projection = {};
-    D3::Transform transform = {};
-};
