0d5f70c65a53b8e30977664371f5a37c4183a138
tomocy <tomocy.dev@gmail.com>
2024-03-07 17:09:16 +0900

    Replace IndexedPrimitive3D with D3.IndexedPrimitive

--- swift-metris/Primitive.swift
+++ swift-metris/Primitive.swift
@@ -35,6 +35,13 @@ extension D3.IndexedPrimitive {
     }
 }
 
+protocol IndexedPrimitive3DAppendable {
+    associatedtype Precision: DimensionalPrecision
+    typealias Primitive = D3.IndexedPrimitive<Precision>
+
+    func append(to primitive: inout Primitive)
+}
+
 extension D3.IndexedPrimitive: MTLRenderCommandEncodableToIndexedAt {
     func encode(with encoder: MTLRenderCommandEncoder, to buffer: MTLIndexedBuffer, at index: Int) {
         if (vertices.isEmpty) {
@@ -64,74 +71,5 @@ extension D3.IndexedPrimitive: MTLRenderCommandEncodableToIndexedAt {
                 indexBufferOffset: 0
             )
         })
-    }
-}
-
-
-struct IndexedPrimitive3D {
-    var vertices: [Vertex] = []
-    var indices: [Index] = []
-
-    var lastEndIndex: Index? {
-        let count = vertices.count
-        return count >= 1
-            ? .init(count - 1)
-            : nil
-    }
-    var nextStartIndex: Index {
-        if let end = lastEndIndex {
-            return end + 1
-        }
-
-        return 0
-    }
-}
-
-extension IndexedPrimitive3D {
-    typealias Vertex = D3.Vertex<Float>
-    typealias Index = UInt16
-}
-
-extension IndexedPrimitive3D {
-    mutating func append(vertices: [Vertex], indices: [Index]) {
-        self.vertices += vertices
-        self.indices += indices
-    }
-}
-
-protocol IndexedPrimitive3DAppendable {
-    associatedtype Precision: DimensionalPrecision
-    typealias Primitive = D3.IndexedPrimitive<Precision>
-
-    func append(to primitive: inout Primitive)
-}
-
-extension IndexedPrimitive3D: MTLRenderCommandEncodableToIndexedAt {
-    func encode(with encoder: MTLRenderCommandEncoder, to buffer: MTLIndexedBuffer, at index: Int) {
-        if (vertices.isEmpty) {
-            return
-        }
-
-        do {
-            buffer.data.contents().copyMemory(
-                from: vertices,
-                byteCount: buffer.data.length
-            )
-            encoder.setVertexBuffer(buffer.data, offset: 0, index: index)
-        }
-
-        do {
-            buffer.index.contents().copyMemory(
-                from: indices,
-                byteCount: buffer.index.length
-            )
-            encoder.drawIndexedPrimitives(
-                type: .triangle,
-                indexCount: indices.count,
-                indexType: .uint16,
-                indexBuffer: buffer.index,
-                indexBufferOffset: 0
-            )
-        }
     }
 }
