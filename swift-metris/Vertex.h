b157b5bce99b104d1d60b426269f0433426aaf95
tomocy <tomocy.dev@gmail.com>
2024-03-07 16:38:07 +0900

    Add D3.Vertex

--- swift-metris/Vertex.h
+++ swift-metris/Vertex.h
@@ -4,6 +4,24 @@
 
 #include "Transform.h"
 
+namespace D3 {
+struct Vertex {
+public:
+    float4 resolvePosition(const float w = 1) const constant {
+        return applyTransformTo(float4(position, w));
+    }
+
+    float4 applyTransformTo(const float4 position) const constant {
+        return transform.apply(position);
+    }
+
+public:
+    float3 position = float3(0, 0, 0);
+    float4 color = float4(0, 0, 0, 1);
+    Transform transform = {};
+};
+};
+
 struct Vertex3D {
 public:
     float4 resolvePosition(const float w = 1) const constant {
--- swift-metris/Vertex.swift
+++ swift-metris/Vertex.swift
@@ -2,6 +2,60 @@
 
 import CoreGraphics
 
+extension D3 {
+    struct Vertex<Precision: DimensionalPrecision> {
+        var position: Measure = .init(0, 0, 0)
+        var color: SIMD4<Float> = .init(0, 0, 0, 1)
+        var transform: Transform = .init()
+    }
+}
+
+extension D3.Vertex {
+    typealias Measure = D3.Storage<Precision>
+    typealias Transform = D3.Transform<Precision>
+}
+
+extension D3.Vertex {
+    init(at position: Measure) {
+        self.position = position
+    }
+}
+
+extension D3.Vertex {
+    mutating func place(at position: Measure) {
+        self.position = position
+    }
+
+    func placed(at position: Measure) -> Self {
+        return mapState(self) { $0.place(at: position) }
+    }
+
+    mutating func colorize(with color: SIMD4<Float>) {
+        self.color = color
+    }
+
+    func colorized(with color: SIMD4<Float>) -> Self {
+        return mapState(self) { $0.colorize(with: color) }
+    }
+
+    mutating func transform(with transform: Transform) {
+        self.transform = transform
+    }
+
+    mutating func transform(by delta: Transform) {
+        transform.transform(by: delta)
+    }
+
+    func transformed(with transform: Transform) -> Self {
+        return mapState(self) { $0.transform(with: transform) }
+    }
+
+    func transformed(by delta: Transform) -> Self {
+        return mapState(self) { $0.transform(by: delta) }
+    }
+}
+
+
 struct Vertex3D {
     var position: SIMD3<Float> = .init(0, 0, 0)
     var color: SIMD4<Float> = .init(0, 0, 0, 1)
