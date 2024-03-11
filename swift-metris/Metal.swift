790b4c1be8f7015bb6c121393880103f977a0bf1
tomocy <tomocy.dev@gmail.com>
2024-03-11 15:55:03 +0900

    Add MTLRenderCommandEncodableAs

--- swift-metris/Metal.swift
+++ swift-metris/Metal.swift
@@ -52,12 +52,47 @@ protocol MTLRenderPipelineDescriable {
 // It should use the buffer if its size matches the encoding one.
 // Otherwise it should make a new buffer, use and retain it for subsequent calls.
 //
+protocol MTLFrameRenderCommandEncodableAs {
+    mutating func encode(
+        with encoder: MTLRenderCommandEncoder,
+        as descriptor: MTLRenderPipelineDescriptor, 
+        in frame: MTLRenderFrame
+    )
+}
+
+protocol MTLFrameRenderCommandEncodableAsAt {
+    mutating func encode(
+        with encoder: MTLRenderCommandEncoder,
+        as descriptor: MTLRenderPipelineDescriptor,
+        at index: Int,
+        in frame: MTLRenderFrame
+    )
+}
+
 protocol MTLFrameRenderCommandEncodable {
     mutating func encode(with encoder: MTLRenderCommandEncoder, in frame: MTLRenderFrame)
 }
 
 protocol MTLFrameRenderCommandEncodableAt {
     mutating func encode(with encoder: MTLRenderCommandEncoder, at index: Int, in frame: MTLRenderFrame)
+}
+
+protocol MTLRenderCommandEncodableAsToAt {
+    func encode(
+        with encoder: MTLRenderCommandEncoder,
+        as descriptor: MTLRenderPipelineDescriptor,
+        to buffer: MTLBuffer,
+        at index: Int
+    )
+}
+
+protocol MTLRenderCommandEncodableAsToIndexedAt {
+    func encode(
+        with encoder: MTLRenderCommandEncoder,
+        as descriptor: MTLRenderPipelineDescriptor,
+        to buffer: MTLIndexedBuffer,
+        at index: Int
+    )
 }
 
 protocol MTLRenderCommandEncodableToAt {
