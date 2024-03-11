82246ba5229bd5ac01d2a95a874f27d72a08bc1f
tomocy <tomocy.dev@gmail.com>
2024-03-11 16:42:06 +0900

    Fix

--- swift-metris/Raster.h
+++ swift-metris/Raster.h
@@ -5,6 +5,6 @@
 #include "Dimension.h"
 
 struct Raster {
-    D3::Coordinate position [[position]] = { 0 };
-    float4 color = float4(0);
+    D3::Coordinate position [[position]] = {0};
+    float4 color = {0, 0, 0, 1};
 };
--- swift-metris/Vertex.h
+++ swift-metris/Vertex.h
@@ -16,8 +16,8 @@ public:
     }
 
 public:
-    Measure position = { 0, 0, 0 };
-    float4 color = float4(0, 0, 0, 1);
+    Measure position = {0, 0, 0};
+    float4 color = {0, 0, 0, 1};
     Transform transform = {};
 };
 }
