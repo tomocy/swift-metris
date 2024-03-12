07dc5013bcb8fbe1623ba5a8e251b462765d4a1c
tomocy <tomocy.dev@gmail.com>
2024-03-12 15:52:01 +0900

    Enable Cube to append itself to IndexedPrimitive<D3.Vertex<Float>>

--- swift-metris/Cube.swift
+++ swift-metris/Cube.swift
@@ -1,6 +1,7 @@
 // tomocy
 
 import CoreGraphics
+import Metal
 
 struct Cube {
     var size: CGVolume = .init(width: 0, height: 0, depth: 0)
@@ -10,6 +11,65 @@ struct Cube {
 
 extension Cube {
     typealias Transform = D3.Transform<Float>
+}
+
+extension Cube {
+    func append(to primitive: inout IndexedPrimitive<D3.Vertex<Float>>) {
+        typealias Primitive = IndexedPrimitive<D3.Vertex<Float>>
+
+        var vertices: [Primitive.Vertex] = []
+        do {
+            let halfSize = Primitive.Vertex.Measure.init(size) / 2
+            vertices += [
+                // front
+                /* 0 */ .init(at: .init(-halfSize.x, halfSize.y, -halfSize.z)),
+                /* 1 */ .init(at: .init(halfSize.x, halfSize.y, -halfSize.z)),
+                /* 2 */ .init(at: .init(halfSize.x, -halfSize.y, -halfSize.z)),
+                /* 3 */ .init(at: .init(-halfSize.x, -halfSize.y, -halfSize.z)),
+                // back
+                /* 4 */ .init(at: .init(halfSize.x, halfSize.y, halfSize.z)),
+                /* 5 */ .init(at: .init(-halfSize.x, halfSize.y, halfSize.z)),
+                /* 6 */ .init(at: .init(-halfSize.x, -halfSize.y, halfSize.z)),
+                /* 7 */ .init(at: .init(halfSize.x, -halfSize.y, halfSize.z)),
+            ]
+
+            vertices = vertices.map({
+                $0.colorized(with: .init(color))
+            }).map({
+                $0.transformed(with: transform)
+            })
+        }
+
+        var indices: [Primitive.Index] = []
+        do {
+            let start = primitive.nextStartIndex
+            indices += [
+                // front
+                start, start + 1, start + 2,
+                start + 2, start + 3, start,
+                // back
+                start + 4, start + 5, start + 6,
+                start + 6, start + 7, start + 4,
+                // left
+                start + 1, start + 4, start + 7,
+                start + 7, start + 2, start + 1,
+                // right
+                start + 5, start, start + 3,
+                start + 3, start + 6, start + 5,
+                // top
+                start + 1, start, start + 5,
+                start + 5, start + 4, start + 1,
+                // bottom
+                start + 7, start + 6, start + 3,
+                start + 3, start + 2, start + 7,
+            ]
+        }
+
+        primitive.append(
+            vertices: vertices,
+            indices: indices
+        )
+    }
 }
 
 extension Cube: IndexedPrimitive3DAppendable {
