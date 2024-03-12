d194761331da17ddbd3fe8775c1fc7339e329e14
tomocy <tomocy.dev@gmail.com>
2024-03-12 17:04:22 +0900

    Let Array where Element: Writable implement Writable

--- swift-metris/Array.swift
+++ swift-metris/Array.swift
@@ -3,3 +3,11 @@
 extension Array {
     var size: Int { MemoryLayout<Element>.stride * count }
 }
+
+extension Array: Writable where Element: Writable {
+    func write(to destination: UnsafeMutableRawPointer) {
+        for (i, v) in self.enumerated() {
+            v.write(to: destination + MemoryLayout<Element>.stride * i)
+        }
+    }
+}
