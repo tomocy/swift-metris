1d75ed7e8832c0570eb838ebd9df3960fa9e0818
tomocy <tomocy.dev@gmail.com>
2024-03-12 15:40:05 +0900

    Add IndexedPrimitive

--- swift-metris/Primitive.swift
+++ swift-metris/Primitive.swift
@@ -2,6 +2,72 @@
 
 import Metal
 
+struct IndexedPrimitive<V> {
+    var vertices: [Vertex] = []
+    var indices: [Index] = []
+}
+
+extension IndexedPrimitive {
+    typealias Vertex = V
+    typealias Index = UInt16
+}
+
+extension IndexedPrimitive {
+    var lastEndIndex: Index? {
+        let count = vertices.count
+        return count >= 1
+            ? .init(count - 1)
+            : nil
+    }
+
+    var nextStartIndex: Index {
+        if let end = lastEndIndex {
+            return end + 1
+        }
+
+        return 0
+    }
+}
+
+extension IndexedPrimitive {
+    mutating func append(vertices: [Vertex], indices: [Index]) {
+        self.vertices += vertices
+        self.indices += indices
+    }
+}
+
+extension IndexedPrimitive: MTLRenderCommandEncodableToIndexedAt {
+    func encode(
+        with encoder: MTLRenderCommandEncoder,
+        to buffer: MTLIndexedBuffer,
+        at index: Int
+    ) {
+        vertices.withUnsafeBytes { body in
+            buffer.data.contents().copyMemory(
+                from: body.baseAddress!,
+                byteCount: buffer.data.length
+            )
+
+            encoder.setVertexBuffer(buffer.data, offset: 0, index: index)
+        }
+
+        indices.withUnsafeBytes { body in
+            buffer.index.contents().copyMemory(
+                from: body.baseAddress!,
+                byteCount: buffer.index.length
+            )
+
+            encoder.drawIndexedPrimitives(
+                type: .triangle,
+                indexCount: indices.count,
+                indexType: .uint16,
+                indexBuffer: buffer.index,
+                indexBufferOffset: 0
+            )
+        }
+    }
+}
+
 extension D3 {
     struct IndexedPrimitive<Precision: DimensionalPrecision> {
         var vertices: [Vertex] = []
