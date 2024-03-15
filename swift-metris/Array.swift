6c469168132818a943a23ea8f36ec1c92822490d
tomocy <tomocy.dev@gmail.com>
2024-03-15 19:39:07 +0900

    Add Array.write(to:)

--- swift-metris/Array.swift
+++ swift-metris/Array.swift
@@ -4,6 +4,14 @@ extension Array {
     var size: Int { MemoryLayout<Element>.stride * count }
 }
 
+extension Array {
+    func write(to destination: UnsafeMutableRawPointer) {
+        for (i, v) in self.enumerated() {
+            IO.write(v, to: destination + MemoryLayout<Element>.stride * i)
+        }
+    }
+}
+
 extension Array: IO.Writable where Element: IO.Writable {
     func write(to destination: UnsafeMutableRawPointer) {
         for (i, v) in self.enumerated() {
