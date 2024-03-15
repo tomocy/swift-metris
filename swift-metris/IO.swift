cffaf5f34e5b183b7fa48ef014192f9c8079b3dc
tomocy <tomocy.dev@gmail.com>
2024-03-15 19:33:14 +0900

    Add IO.write(_:to:)

--- swift-metris/IO.swift
+++ swift-metris/IO.swift
@@ -6,15 +6,21 @@ enum IO {
     typealias Writable = _Writable
 }
 
+extension IO {
+    static func write<T>(_ target: T, to destination: UnsafeMutableRawPointer) {
+        withUnsafeBytes(of: target) { bytes in
+            destination.copy(from: bytes.baseAddress!, count: bytes.count)
+        }
+    }
+}
+
 protocol _Writable {
     func write(to destination: UnsafeMutableRawPointer)
 }
 
-extension IO.Writable where Self: FixedWidthInteger {
+extension IO.Writable {
     func write(to destination: UnsafeMutableRawPointer) {
-        withUnsafeBytes(of: self) { bytes in
-            destination.copy(from: bytes.baseAddress!, count: bytes.count)
-        }
+        IO.write(self, to: destination)
     }
 }
 
