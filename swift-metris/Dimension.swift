339263857e92b23077f5ecaaca19302906a9fd7f
tomocy <tomocy.dev@gmail.com>
2024-03-17 15:50:38 +0900

    Add D3.Axis

--- swift-metris/Dimension.swift
+++ swift-metris/Dimension.swift
@@ -13,6 +13,11 @@ extension Float: Dimension.Precision {}
 enum D3 {}
 
 extension D3 {
+    enum Axis {
+        case x, y, z
+    }
+
+
     typealias Storage = SIMD3
     typealias Matrix = float4x4
 }
