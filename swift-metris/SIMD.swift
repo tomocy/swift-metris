4e471621fb02e61cdd03753143b8e99a18b5f403
tomocy <tomocy.dev@gmail.com>
2024-03-17 19:40:48 +0900

    Add SIMD3<Float>.Packed

--- swift-metris/SIMD.swift
+++ swift-metris/SIMD.swift
@@ -1,5 +1,7 @@
 // tomocy
 
+import Metal
+
 extension SIMD3 {
     init(filled value: Scalar) {
         self.init(value, value, value)
@@ -13,5 +15,21 @@ extension SIMD3<Float> {
             .init(volume.height),
             .init(volume.depth)
         )
+    }
+}
+
+extension SIMD3<Float> {
+    typealias Packed = MTLPackedFloat3
+}
+
+extension MTLPackedFloat3 {
+    init(_ relaxed: SIMD3<Float>) {
+        self.init(
+            .init(elements: (relaxed.x, relaxed.y, relaxed.z))
+        )
+    }
+
+    init(_ x: Float, _ y: Float, _ z: Float) {
+        self.init(.init(x, y, z))
     }
 }
