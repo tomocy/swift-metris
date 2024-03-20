5f1bca6a030daffefb83f04870034a90381d2a6e
tomocy <tomocy.dev@gmail.com>
2024-03-21 00:16:43 +0900

    Add SIMD4.xyz

--- swift-metris/SIMD.swift
+++ swift-metris/SIMD.swift
@@ -1,10 +1,17 @@
 // tomocy
 
+import simd
 import Metal
 
 extension SIMD3 {
     init(filled value: Scalar) {
         self.init(value, value, value)
+    }
+}
+
+extension SIMD3 {
+    init(_ other: SIMD4<Scalar>) {
+        self.init(other.x, other.y, other.z)
     }
 }
 
@@ -31,5 +38,17 @@ extension MTLPackedFloat3 {
 
     init(_ x: Float, _ y: Float, _ z: Float) {
         self.init(.init(x, y, z))
+    }
+}
+
+extension SIMD4 {
+    var xyz: SIMD3<Scalar> { .init(self) }
+}
+
+extension float4x4 {
+    init(columns: [SIMD4<Float>]) {
+        self.init(
+            columns: (columns[0], columns[1], columns[2], columns[3])
+        )
     }
 }
