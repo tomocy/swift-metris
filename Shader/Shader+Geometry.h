defc0745ce49552adaec27f7b66d98574b9f46a9
tomocy <tomocy.dev@gmail.com>
2024-03-22 20:55:54 +0900

    Add Shader/D3.Positions.{InNDC, InClip, InView, InWorld}

--- Shader/Shader+Geometry.h
+++ Shader/Shader+Geometry.h
@@ -19,6 +19,32 @@ using Matrix = metal::float4x4;
 }
 
 namespace D3 {
+namespace Positions {
+
+struct InNDC {
+public:
+    Measure value;
+};
+
+struct InClip {
+public:
+    Coordinate value;
+};
+
+struct InView {
+public:
+    Coordinate value;
+};
+
+struct InWorld {
+public:
+    Coordinate value;
+};
+
+}
+}
+
+namespace D3 {
 
 struct Aspect {
 public:
--- Shader/Shader+Geometry.swift
+++ Shader/Shader+Geometry.swift
@@ -5,10 +5,30 @@ import simd
 extension Shader {
     enum D3 {
         typealias Measure = SIMD3<Float>
+        typealias Coordinate = SIMD4<Float>
         typealias Matrix = float4x4
     }
 }
 
+extension Shader.D3 {
+    enum Positions {
+        struct InNDC {
+            var value: Measure
+        }
+
+        struct InClip {
+            var value: Coordinate
+        }
+
+        struct InView {
+            var value: Coordinate
+        }
+
+        struct InWorld {
+            var value: Coordinate
+        }
+    }
+}
 
 extension Shader.D3 {
     struct Aspect {
