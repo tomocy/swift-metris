731e5607c81b91b0ddc5331072da19d4102c4de5
tomocy <tomocy.dev@gmail.com>
2024-03-23 22:25:07 +0900

    Make Farm.World implement Shader.D3.Shadow.Encodable

--- Farm/Farm+Farm.swift
+++ Farm/Farm+Farm.swift
@@ -12,3 +12,17 @@ extension Farm {
         var ground: Mesh
     }
 }
+
+extension Farm.World: Shader.D3.Shadow.Encodable {
+    func encode(with encoder: Shader.D3.Shadow.Encoder) {
+        lights.directional.encode(with: encoder.raw)
+
+        do {
+            spots.encode(with: encoder.raw)
+            ground.encode(with: encoder.raw)
+        }
+        do {
+            ground.encode(with: encoder.raw)
+        }
+    }
+}
