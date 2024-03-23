9a29bb37110b614a94113b6421852339a4bc87b4
tomocy <tomocy.dev@gmail.com>
2024-03-23 22:19:35 +0900

    Add Farm/World.{camera, lights}

--- Farm/Farm+Farm.swift
+++ Farm/Farm+Farm.swift
@@ -4,6 +4,9 @@ enum Farm {}
 
 extension Farm {
     struct World {
+        var camera: Camera
+        var lights: Lights
+
         var spots: Mesh
         var monolith: Mesh
         var ground: Mesh
