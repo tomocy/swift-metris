f41ece5c3bb6c5c108c4920c9f5c9d137cf7a96c
tomocy <tomocy.dev@gmail.com>
2024-03-23 22:06:55 +0900

    Add Farm/Lights.Directional

--- Farm/Farm+Light.swift
+++ Farm/Farm+Light.swift
@@ -1,12 +1,20 @@
 // tomocy
 
 extension Farm {
-    enum Lights {}
+    struct Lights {}
 }
 
 extension Farm.Lights {
     struct Ambient {
         var color: SIMD3<Float>
         var intensity: Float
+    }
+}
+
+extension Farm.Lights {
+    struct Directional {
+        var color: SIMD3<Float>
+        var intensity: Float
+        var direction: SIMD3<Float>
     }
 }
