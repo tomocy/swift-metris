7157b4b3b71205a94c4105023beb6ad5b1b796ec
tomocy <tomocy.dev@gmail.com>
2024-03-24 17:44:00 +0900

    Add Metris/World.lights

--- Metris/Metris+Metris.swift
+++ Metris/Metris+Metris.swift
@@ -14,8 +14,28 @@ enum MetrisX {
                     translate: .init(0, 0.5, -2)
                 )
             )
+
+            lights = .init(
+                ambient: .init(
+                    color: .init(1, 1, 1),
+                    intensity: 0.1
+                ),
+                directional: .init(
+                    color: .init(1, 1, 1),
+                    intensity: 0.8,
+                    direction: .init(-1, -1, 1)
+                ),
+                point: .init(
+                    color: .init(0.95, 0, 0),
+                    intensity: 0.8,
+                    transform: .init(
+                        translate: .init(-0.5, 1, -0.5)
+                    )
+                )
+            )
         }
 
         var camera: Engine.D3.Camera
+        var lights: Engine.D3.Lights
     }
 }
