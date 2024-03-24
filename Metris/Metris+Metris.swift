833203fc3769de5c3575801959165285f715432b
tomocy <tomocy.dev@gmail.com>
2024-03-24 17:43:22 +0900

    Add Metris/World.camera

--- Metris/Metris+Metris.swift
+++ Metris/Metris+Metris.swift
@@ -1,5 +1,21 @@
 // tomocy
 
 enum MetrisX {
-    class World {}
+    class World {
+        init(resolution: SIMD2<Float>) {
+            assert(resolution.x != 0 && resolution.y != 0)
+
+            camera = .init(
+                projection: Engine.D3.Transform.perspective(
+                    near: 0.01, far: 100,
+                    aspectRatio: resolution.x / resolution.y, fovY: .pi / 3
+                ),
+                transform: .init(
+                    translate: .init(0, 0.5, -2)
+                )
+            )
+        }
+
+        var camera: Engine.D3.Camera
+    }
 }
