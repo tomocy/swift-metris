6ac95c87db51de1a89da71aa8ef80a285b6594b4
tomocy <tomocy.dev@gmail.com>
2024-03-24 15:24:43 +0900

    Add Engine/D3.Transform.{orthogonal, perspective}

--- Engine/Engine+Transform.swift
+++ Engine/Engine+Transform.swift
@@ -11,6 +11,51 @@ extension Engine.D3 {
 }
 
 extension Engine.D3.Transform {
+    static func orthogonal(
+        top: Float, bottom: Float,
+        left: Float, right: Float,
+        near: Float, far: Float
+    ) -> float4x4 {
+        // Note that
+        // in Metal, unlike x and y axes where values are mapped into -1...1,
+        // values on z-axis is into 0...1.
+
+        let translate: SIMD3<Float> = .init(
+            (right + left) / (right - left),
+            (top + bottom) / (top - bottom),
+            near / (far - near)
+        )
+
+        let scale: SIMD3<Float> = .init(
+            (1 - -1)  / (right - left),
+            (1 - -1) / (top - bottom),
+            (1 - 0) / (far - near)
+        )
+
+        return Self.translate(translate)
+            * Self.scale(scale)
+    }
+
+    static func perspective(
+        near: Float, far: Float,
+        aspectRatio: Float, fovY: Float
+    ) -> float4x4 {
+        var scale = SIMD2<Float>.init(1, 1)
+        scale.y = 1 / tan(fovY / 2)
+        scale.x = scale.y * (1 / aspectRatio) // 1 / w = (1 / h) * (h / w)
+
+        return .init(
+            rows: [
+                .init(scale.x, 0, 0, 0),
+                .init(0, scale.y, 0, 0),
+                .init(0, 0, far / (far - near), -(far * near) / (far - near)),
+                .init(0, 0, 1, 0)
+            ]
+        )
+    }
+}
+
+extension Engine.D3.Transform {
     func resolved() -> float4x4 {
         return Self.translate(translate)
             * Self.rotate(rotate)
