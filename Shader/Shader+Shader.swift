69d94f4467022b83b8e4ad44752b562ebefdcffc
tomocy <tomocy.dev@gmail.com>
2024-03-25 15:42:14 +0900

    Add Shader/RenderContext

--- Shader/Shader+Shader.swift
+++ Shader/Shader+Shader.swift
@@ -4,6 +4,15 @@ import Metal
 
 enum Shader {}
 
+extension Shader {
+    typealias RenderContext = _ShaderRenderContext
+}
+
+protocol _ShaderRenderContext {
+    var encoder: any MTLRenderCommandEncoder { get }
+    var buffers: Shader.Buffers.Framed { get }
+}
+
 extension Shader.D3 {
     struct Shader {
         var commandQueue: MTLCommandQueue
